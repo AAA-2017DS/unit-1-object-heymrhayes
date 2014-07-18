6c9acab90d4db2df7b0c6cfa8b67f6a4e7cdb043
Geoff Schmit <gcschmit@gmail.com>
2014-07-17 23:25:39 -0500

    finished my lab

---
+++ TurtleLab/TurtleLab.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class TurtleLab
+{
+    public static void main(String args[])
+    {
+        World turtleWorld = new World();
+        Turtle turtle = new Turtle(turtleWorld);
+        turtle.setPenColor(Color.BLUE);
+        turtle.penDown();
+        turtle.forward(50);
+    }
+}
